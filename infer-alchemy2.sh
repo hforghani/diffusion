6eff1565836bdd306355462de97606cbe34628a3
h_forghani <forghani@arsh.co.ir>
2018-11-05 18:10:40 +0330

    Shell script added for alchemy2 inference.

---
+++ infer-alchemy2.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+# usage: ./infer-alchemy2.sh project meme1 [meme2 ...]
+project=$1
+
+while true
+do
+
+    meme=$2
+    if [ "$meme" = "" ]
+    then
+        break
+    fi
+
+    echo "running inference on " $meme "..."
+    ~/social/alchemy-2/bin/infer -i data/$project/weights-$project-alchemy2.mln \
+                                 -e data/$project/evidence-alchemy2/ev-test-$project-alchemy2-m$meme.db \
+                                 -r data/$project/results-alchemy2/results-$project-alchemy2-m$meme.results \
+                                 -q isActivated \
+                                 -maxSteps 1000
+                                 2>&1 | tee log/infer-alchemy2-m$meme.log
+    shift
+
+done
