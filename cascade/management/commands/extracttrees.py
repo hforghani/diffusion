9010057836b8c84c60fde5b0e782eeca9de1ee25
UBUNTU1804_001 <forghani@arsh.co.ir>
2018-11-29 19:35:01 +0330

    extracttrees multi-processing disabled. A requirement added.

--- cascade/management/commands/extracttrees.py
+++ cascade/management/commands/extracttrees.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import json
+import sys
 from multiprocessing.pool import Pool
 import os
 import traceback
@@ -17,20 +18,23 @@ from cascade.models import CascadeTree
 from crud.models import Meme
 
 
-def extract_cascades(meme_ids, process_num):
-    t0 = time.time()
-    trees = {}
-    i = 0
+def extract_cascades(meme_ids, process_num, stdout=sys.stdout):
+    try:
+        t0 = time.time()
+        trees = {}
+        i = 0
 
-    for meme_id in meme_ids:
-        i += 1
-        tree = CascadeTree().extract_cascade(meme_id).get_dict()
-        trees[meme_id] = tree
-        if i % 1000 == 0:
-            print('process {}: {} memes done: {:.0f} s'.format(process_num, i, time.time() - t0))
-            t0 = time.time()
+        for meme_id in meme_ids:
+            i += 1
+            tree = CascadeTree().extract_cascade(meme_id).get_dict()
+            trees[meme_id] = tree
+            if i % 100 == 0:
+                stdout.write('process {}: {} memes done: {:.0f} s'.format(process_num, i, time.time() - t0))
+                t0 = time.time()
 
-    return trees
+        return trees
+    except:
+        traceback.print_exc()
 
 
 class Command(BaseCommand):
@@ -49,7 +53,10 @@ class Command(BaseCommand):
             if os.path.exists(path):
                 self.stdout.write('loading trees ...')
                 trees = json.load(open(path, 'r'))
-                trees = {int(key): value for key, value in trees.items()}
+                if trees is not None:
+                    trees = {int(key): value for key, value in trees.items()}
+                else:
+                    trees = {}
 
             # Check if all meme trees are in trees dictionary. Extract cascade tree for the ones which not exist.
             self.stdout.write('checking if any tree not extracted ...')
@@ -58,26 +65,30 @@ class Command(BaseCommand):
                 meme_ids = list(set(meme_ids) - set(trees.keys()))
                 self.stdout.write('extracting cascade trees for %d memes ...' % len(meme_ids))
 
-                # Distribute the extraction into some simultaneous processes.
-                process_count = 2
-                step = int(math.ceil(len(meme_ids) / process_count))
-                pool = Pool(processes=process_count)
-                results = []
+                # # Distribute the extraction into some simultaneous processes.
+                # process_count = 3
+                # step = int(math.ceil(len(meme_ids) / process_count))
+                # pool = Pool(processes=process_count)
+                # results = []
+                #
+                # for process_num in range(1, process_count + 1):
+                #     cur_meme_ids = meme_ids[(process_num - 1) * step: process_num * step]
+                #     res = pool.apply_async(extract_cascades, (cur_meme_ids, process_num))
+                #     self.stdout.write('process {} created'.format(process_num))
+                #     results.append(res)
+                #
+                # pool.close()
+                # pool.join()
+                #
+                # # Gather and add up the results of the processes.
+                # for res in results:
+                #     cur_trees = res.get()
+                #     trees.update(cur_trees)
 
-                for process_num in range(1, step + 1):
-                    cur_meme_ids = meme_ids[(process_num - 1) * step: process_num * step]
-                    res = pool.apply_async(extract_cascades, (cur_meme_ids, process_num))
-                    results.append(res)
-
-                pool.close()
-                pool.join()
-
-                # Gather and add up the results of the processes.
-                for res in results:
-                    cur_trees = res.get()
-                    trees.update(cur_trees)
+                trees = extract_cascades(meme_ids, 1, self.stdout)
 
                 # Save the results into the file.
+                self.stdout.write('writing to file ...')
                 json.dump(trees, open(path, 'w'), indent=1)
 
             self.stdout.write('command done in %f min' % ((time.time() - start) / 60))
