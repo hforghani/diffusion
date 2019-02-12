1a55a16534570802f3e8cb627d4f4c65fcb500c3
h_forghani <forghani@arsh.co.ir>
2019-02-13 01:39:47 +0330

    memedepths and extract_cascades method changed to use mongodb.

--- crud/management/commands/memedepths.py
+++ crud/management/commands/memedepths.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
 import json
-import math
-import multiprocessing
 import os
 import traceback
 import django
@@ -15,6 +13,7 @@ if not apps.ready and not settings.configured:
 
 from cascade.models import CascadeTree
 from crud.models import Meme
+from crud.mongo import mongodb
 
 
 class Command(BaseCommand):
@@ -48,38 +47,61 @@ class Command(BaseCommand):
             raise
 
     def calc_depths(self, just_null=False):
-        i = 0
-        t0 = time.time()
-        memes = Meme.objects
-        if just_null:
-            memes = memes.filter(depth__isnull=True)
-        self.stdout.write('number of memes to calculate depths = %d' % memes.count())
-        for meme in memes.iterator():
-            tree = CascadeTree().extract_cascade(meme.id)
-            meme.depth = tree.depth
-            meme.save()
-            i += 1
-            if i % 100 == 0:
-                self.stdout.write('%d memes done. mean time: %.2f s' % (i, (time.time() - t0) / i * 100))
+        query = {'depth': None} if just_null else {}
+        memes = mongodb.memes.find(query, {'_id': 1})
 
-    def set_depths_by_trees_data(self, trees_path):
-        self.stdout.write('loading trees ...')
-        with open(trees_path, 'r') as f:
-            i = 0
-            json_str = '{'
-            for line in f:
-                if line in ['{\n', '}\n', '}']:
-                    continue
-                line = line.strip()
-                if line != '],':
-                    json_str += line
+        count = memes.count()
+        self.stdout.write('number of memes to calculate depths = %d' % count)
+        i = 0
+        step = 1000
+        t0 = time.time()
+        times = []
+
+        for meme in memes:
+            tree = CascadeTree().extract_cascade(meme['_id'])
+            mongodb.memes.find_one_and_update({'_id': meme['_id']}, {'$set': {'depth': tree.depth}})
+
+            i += 1
+            if i % step == 0:
+                t = time.time() - t0
+                if len(times) >= 10:
+                    times.pop(0)
+                times.append(t)
+
+                avg = sum(times) / len(times)
+                rem = (count - i) * avg / (60 * step)
+
+                if rem > 60 * 48:
+                    rem_str = '{:.0f} days'.format(rem / (60 * 24))
+                elif rem > 60:
+                    rem_str = '{:.0f} hours'.format(rem / 60)
                 else:
-                    json_str += ']}'
-                    data = json.loads(json_str)
-                    meme_id = int(list(data.keys())[0])
-                    tree = CascadeTree().from_dict(list(data.values())[0])
-                    Meme.objects.filter(id=meme_id).update(depth=tree.depth)
-                    i += 1
-                    if i % 100 == 0:
-                        self.stdout.write('%d memes done' % i)
-                    json_str = '{'
+                    rem_str = '{:.0f} minutes'.format(rem)
+
+                self.stdout.write(
+                    '{} memes done. mean time: {:.0f} s per {}. estimated remaining time: {}'
+                    .format(i, avg, step, rem_str))
+                t0 = time.time()
+
+
+def set_depths_by_trees_data(self, trees_path):
+    self.stdout.write('loading trees ...')
+    with open(trees_path, 'r') as f:
+        i = 0
+        json_str = '{'
+        for line in f:
+            if line in ['{\n', '}\n', '}']:
+                continue
+            line = line.strip()
+            if line != '],':
+                json_str += line
+            else:
+                json_str += ']}'
+                data = json.loads(json_str)
+                meme_id = int(list(data.keys())[0])
+                tree = CascadeTree().from_dict(list(data.values())[0])
+                Meme.objects.filter(id=meme_id).update(depth=tree.depth)
+                i += 1
+                if i % 100 == 0:
+                    self.stdout.write('%d memes done' % i)
+                json_str = '{'
