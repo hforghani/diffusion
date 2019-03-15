a1d6bf3aaef8320513f4a856b75a71d3c1b7ca61
h_forghani <forghani@arsh.co.ir>
2019-03-15 13:59:24 +0330

    Bug solved from setpostmemesdt.

--- setpostmemesdt.py
+++ setpostmemesdt.py
@@ -2,16 +2,14 @@ from mongo import mongodb
 
 post_ids = mongodb.postmemes.find({'datetime': None}, {'post_id': 1, '_id': 0})
 post_ids = list({p['post_id'] for p in post_ids})
-datetimes = mongodb.posts.find({'_id': {'$in': post_ids}}, ['datetime'])
-datetimes = {p['_id']: p['datetime'] for p in datetimes}
 
-i = 0
-count = mongodb.postmemes.find({'datetime': None}).count()
-
-for pm in mongodb.postmemes.find({'datetime': None}, ['post_id']):
-    mongodb.memes.find_one_and_update({'_id': pm['_id']}, {'$set': {'datetime': datetimes[pm['post_id']]}})
-    i += 1
-    if i % 1000 == 0:
-        print('{:.0f}% done. {} postmemes updated'.format(i / count * 100, i))
+step = 10 ** 6
+count = len(post_ids)
+for i in range(0, count, step):
+    if i != 0:
+        print('{:.0f}% done. {} datetimes set'.format(i / count * 100, i))
+    posts = mongodb.posts.find({'_id': {'$in': post_ids[i: i + step]}}, ['datetime'])
+    for p in posts:
+        mongodb.postmemes.update_many({'post_id': p['_id']}, {'$set': {'datetime': p['datetime']}})
 
 print('100% done')
