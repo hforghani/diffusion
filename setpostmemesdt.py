be28a2f7c85fb65355a13a70d2828bec3d4209fd
h_forghani <forghani@arsh.co.ir>
2019-03-15 14:27:19 +0330

    setpostmemesdt chnaged to avoid BSON size problem.

--- setpostmemesdt.py
+++ setpostmemesdt.py
@@ -1,15 +1,17 @@
 from mongo import mongodb
 
 post_ids = mongodb.postmemes.find({'datetime': None}, {'post_id': 1, '_id': 0})
-post_ids = list({p['post_id'] for p in post_ids})
+post_ids_done = set()
+count = post_ids.count()
+i = 0
 
-step = 10 ** 6
-count = len(post_ids)
-for i in range(0, count, step):
-    if i != 0:
+for p in post_ids:
+    i += 1
+    if p['post_id'] in post_ids_done:
+        continue
+    dt = mongodb.posts.find_one({'_id': p['post_id']}, ['datetime'])['datetime']
+    mongodb.postmemes.update_many({'post_id': p['_id']}, {'$set': {'datetime': dt}})
+    if i % 1000 == 0:
         print('{:.0f}% done. {} datetimes set'.format(i / count * 100, i))
-    posts = mongodb.posts.find({'_id': {'$in': post_ids[i: i + step]}}, ['datetime'])
-    for p in posts:
-        mongodb.postmemes.update_many({'post_id': p['_id']}, {'$set': {'datetime': p['datetime']}})
 
 print('100% done')
