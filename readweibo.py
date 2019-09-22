64427df7148541c0631a2996561ab3cb36bead3e
h_forghani <forghani@arsh.co.ir>
2019-09-23 01:44:43 +0330

    Reading retweets data from Weibo completed.

--- readweibo.py
+++ readweibo.py
@@ -1,14 +1,11 @@
 # -*- coding: utf-8 -*-
 import argparse
 import logging
-import os
-import re
 import traceback
 import time
 
-from bson import SON
 from bson.objectid import ObjectId
-import pygtrie
+from datetime import timedelta
 from pymongo.errors import BulkWriteError
 from pymongo.operations import UpdateOne
 
@@ -71,6 +68,8 @@ class Command:
                 else:
                     users = mongodb.users.find({}, ['_id', 'username'])
                     users_map = {u['username']: u['_id'] for u in users}
+                    if '' in users_map:
+                        del users_map['']
 
                 # Create memes and their root posts.
                 if args.roots_file:
@@ -182,7 +181,8 @@ class Command:
 
                         if str(user_id) not in user_ids:
                             users.append({'_id': user_id, 'username': username})
-                            users_map[username] = user_id
+                            if username:
+                                users_map[username] = user_id
                             user_ids.add(str(user_id))
 
                             i += 1
@@ -213,6 +213,8 @@ class Command:
         i = 0
         t0 = time.time()
         memes_count = mongodb.memes.count()
+        post_memes = []
+        reshares = []
 
         # if 'start_index' is specified, ignore lower indexes.
         ignoring = False
@@ -223,9 +225,13 @@ class Command:
 
             while True:
                 i += 1
-                reshares = self.read_one_meme_reshares(f, users_map, memes_map)
+                resh_list, pm_list = self.read_one_meme_reshares(f, users_map, memes_map, ignoring)
+                if resh_list is None and pm_list is None:
+                    break
+                reshares.extend(resh_list)
+                post_memes.extend(pm_list)
 
-                if (not ignoring or i == start_index) and i % 10000 == 0:
+                if (not ignoring or i == start_index) and (len(post_memes) > 10000 or len(reshares) > 10000):
                     logger.info(
                         'saving %d post memes and %d reshares ...' % (len(post_memes), len(reshares)))
                     if post_memes or reshares:
@@ -237,7 +243,7 @@ class Command:
                         t0 = time.time()
                     logger.info('{:.0f}% done. processing from post number {} ...'.format(i / memes_count * 100, i))
 
-                elif ignoring and i % 100000 == 0:
+                elif ignoring and i % 100 == 0:
                     logger.info('ignoring posts: %d' % i)
 
                 # Handle if it is in ignoring state.
@@ -254,53 +260,53 @@ class Command:
         mongodb.postmemes.insert_many(post_memes)
         mongodb.reshares.insert_many(reshares)
 
-    def read_one_meme_reshares(self, f, users_map, memes_map):
+    def read_one_meme_reshares(self, f, users_map, memes_map, ignoring=False):
         # Read root post data.
         line = f.readline()
         if line:
             line = line.strip()
         else:
-            return None
+            return None, None
 
         original_pid, original_uid, original_time, _ = line.split()
         original_pid = ObjectId('{:024d}'.format(int(original_pid)))
         original_uid = ObjectId('{:024d}'.format(int(original_uid)))
         original_time = str_to_datetime(original_time, '%Y-%m-%d-%H:%M:%S')
 
-        mongodb.posts.update_one({'_id': original_pid},
-                                 {'$set': {'datetime': original_time, 'author_id': original_uid}})
+        if not ignoring:
+            mongodb.posts.update_one({'_id': original_pid},
+                                     {'$set': {'datetime': original_time, 'author_id': original_uid}})
         meme_id = memes_map[str(original_pid)]
-        uname_data = mongodb.users.find_one({'_id': original_uid}, {'_id': False, 'username': True})
-        original_uname = uname_data['username'] if uname_data else str(original_uid)
-        print('\nreading meme {}'.format(str(meme_id)))
 
         # Read retweets number.
         line = f.readline().strip()
         retweet_num = int(line)
 
-        reshares = []
-        ret_lists = []
+        meme_reshares = []
+        meme_postmemes = []
+        #resh_pairs = set()
+        posts_map = {str(original_uid): {'_id': original_pid, 'datetime': original_time}}
 
         for i in range(retweet_num):
-            ret_list, ret_time = self.read_one_reshare(f, meme_id, users_map, original_uname)
-            ret_lists.append((ret_list, ret_time))
-            #reshares.append(reshare)
+            reshares, post_memes = self.read_one_reshare_seq(f, meme_id, original_uid, users_map, posts_map, ignoring)
+            meme_reshares.extend(reshares)
+            meme_postmemes.extend(post_memes)
 
-        ret_lists = sorted(ret_lists, key=lambda x: x[1])
-        for item in ret_lists:
-            print(str(item[1]) + ' : ' + ' -> '.join(item[0]))
-
-        return reshares
+        return meme_reshares, meme_postmemes
 
 
-    def read_one_reshare(self, f, meme_id, users_map, original_uname):
+    def read_one_reshare_seq(self, f, meme_id, original_uid, users_map, posts_map, ignoring=False):
         # Read retweet data.
-        line = f.readline()
-        if not line:
-            return None
-        line = line.strip()
+        line = None
+        while line is None or line == '\n':
+            line = f.readline()
+            if not line:
+                return None, None
 
-        retweet_uid, retweet_time, retweet_pid = line.split()
+        try:
+            retweet_uid, retweet_time, retweet_pid = line.split()
+        except ValueError:
+            raise
         retweet_pid = ObjectId('{:024d}'.format(int(retweet_pid)))
         retweet_uid = ObjectId('{:024d}'.format(int(retweet_uid)))
         retweet_time = str_to_datetime(retweet_time, '%Y-%m-%d-%H:%M:%S')
@@ -315,26 +321,57 @@ class Command:
             last_pos = f.tell()
             line = f.readline().strip()
 
-        uname_data = mongodb.users.find_one({'_id': retweet_uid}, {'_id': False, 'username': True})
-        uname = uname_data['username'] if uname_data else str(retweet_uid)
-
         # Read retweet list if exists.
         if line[:7] == 'retweet':
-            ret_list = [original_uname] + line[8:].split() + [uname]
-            #anc_ids = [users_map[uname] for uname in ancestors]
+            ret_list = line[8:].split()
+            uid_list = [original_uid]
+
+            for uname in ret_list:
+                if uname in users_map:
+                    uid_list.append(users_map[uname])
+                else:
+                    user_id = mongodb.users.insert_one({'username': uname}).inserted_id
+                    uid_list.append(user_id)
+
+            uid_list.append(retweet_uid)
+
             last_pos = f.tell()
             line = f.readline().strip()
+
         else:
-            ret_list = [original_uname, uname]
+            uid_list = [original_uid, retweet_uid]
 
         if line[:4] != 'link':
             f.seek(last_pos)
 
-        #reshare = {'post_id': retweet_pid, 'reshared_post_id': parent_pid, 'datetime': retweet_time,
-        #           'user_id': retweet_uid, 'ref_user_id': parent_uid, 'ref_datetime': original_time}
-        reshare = {}
+        reshares = []
+        post_memes = []
 
-        return ret_list, retweet_time
+        if not ignoring:
+
+            # Create posts, postmemes, and reshares for the edges not visited before.
+            for i in range(len(uid_list) - 1):
+                resh_dt = retweet_time - timedelta(microseconds=(len(uid_list) - i - 2) * 10)
+                src, dst = uid_list[i], uid_list[i + 1]
+
+                #if (str(src), str(dst)) not in resh_pairs:
+                resh_post = posts_map[str(src)]
+                resh_post_id = resh_post['_id']
+
+                if i == len(uid_list) - 1:
+                    post_id = retweet_pid
+                    mongodb.posts.insert_one({'_id': post_id, 'author_id': dst, 'datetime': resh_dt})
+                else:
+                    post_id = mongodb.posts.insert_one({'author_id': dst, 'datetime': resh_dt}).inserted_id
+
+                resh = {'post_id': post_id, 'reshared_post_id': resh_post_id, 'datetime': resh_dt,
+                        'user_id': dst, 'ref_user_id': src, 'ref_datetime': resh_post['datetime']}
+                reshares.append(resh)
+                post_memes.append({'post_id': post_id, 'meme_id': meme_id})
+                posts_map[str(dst)] = {'_id': post_id, 'datetime': resh_dt}
+                #resh_pairs.add((str(src), str(dst)))
+
+        return reshares, post_memes
 
 
     def calc_memes_values(self):
