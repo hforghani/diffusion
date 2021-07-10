6fecbe6119465d080d80dec32346e3da9009f73e
h_forghani <forghani@arsh.co.ir>
2021-07-10 16:49:46 +0430

    Bug fixed.

--- temp_scripts/set_postmemes_author.py
+++ temp_scripts/set_postmemes_author.py
@@ -19,7 +19,7 @@ if __name__ == '__main__':
         i += 1
         if p['post_id'] in post_ids_done:
             continue
-        author_id = db.posts.find_one({'_id': p['post_id']}, {'author_id': 1, '_id': 0})['c']
+        author_id = db.posts.find_one({'_id': p['post_id']}, {'author_id': 1, '_id': 0})['author_id']
         db.postmemes.update_many({'post_id': p['post_id']}, {'$set': {'author_id': author_id}})
         post_ids_done.add(p['post_id'])
         if i % 1000 == 0:
