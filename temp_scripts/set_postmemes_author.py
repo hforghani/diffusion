7fb74e72befbacda0ddcd649016742458147cde1
h_forghani <forghani@arsh.co.ir>
2021-07-11 15:35:21 +0430

    Logging of 2 scripts changed.

--- temp_scripts/set_postmemes_author.py
+++ temp_scripts/set_postmemes_author.py
@@ -14,6 +14,7 @@ if __name__ == '__main__':
     logger.info('done')
     logger.info('setting authors ...')
     i = 0
+    last_percent = 0
 
     for p in post_ids:
         i += 1
@@ -22,7 +23,8 @@ if __name__ == '__main__':
         author_id = db.posts.find_one({'_id': p['post_id']}, {'author_id': 1, '_id': 0})['author_id']
         db.postmemes.update_many({'post_id': p['post_id']}, {'$set': {'author_id': author_id}})
         post_ids_done.add(p['post_id'])
-        if i % 1000 == 0:
-            logger.info('{:.0f}% done. author_id of {} postmemes set'.format(i / count * 100, i))
+        if i * 100 // count > last_percent:
+            last_percent = i * 100 // count
+            logger.info('{}% done. author_id of {} postmemes set'.format(last_percent, i))
 
     logger.info('100% done')
--- temp_scripts/set_postmemes_dt.py
+++ temp_scripts/set_postmemes_dt.py
@@ -1,20 +1,27 @@
+import sys
+
+sys.path.append('.')
+
 from db.managers import DBManager
 from settings import logger
 
-db = DBManager().db
-post_ids = db.postmemes.find({'datetime': None}, {'post_id': 1, '_id': 0})
-post_ids_done = set()
-count = post_ids.count()
-i = 0
+if __name__ == '__main__':
+    db = DBManager().db
+    post_ids = db.postmemes.find({'datetime': None}, {'post_id': 1, '_id': 0})
+    post_ids_done = set()
+    count = post_ids.count()
+    i = 0
+    last_percent = 0
 
-for p in post_ids:
-    i += 1
-    if p['post_id'] in post_ids_done:
-        continue
-    dt = db.posts.find_one({'_id': p['post_id']}, ['datetime'])['datetime']
-    db.postmemes.update_many({'post_id': p['post_id']}, {'$set': {'datetime': dt}})
-    post_ids_done.add(p['post_id'])
-    if i % 1000 == 0:
-        logger.info('{:.0f}% done. {} datetimes set'.format(i / count * 100, i))
+    for p in post_ids:
+        i += 1
+        if p['post_id'] in post_ids_done:
+            continue
+        dt = db.posts.find_one({'_id': p['post_id']}, ['datetime'])['datetime']
+        db.postmemes.update_many({'post_id': p['post_id']}, {'$set': {'datetime': dt}})
+        post_ids_done.add(p['post_id'])
+        if i * 100 // count > last_percent:
+            last_percent = i * 100 // count
+            logger.info('{}% done. datetime of {} postmemes set'.format(last_percent, i))
 
-logger.info('100% done')
+    logger.info('100% done')
