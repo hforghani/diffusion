e72d59a6a77d342ada4f296617af5e02a6f2941e
h_forghani <forghani@arsh.co.ir>
2021-08-31 17:33:11 +0430

    Some logs added again!

--- cluster/cluster_memes.py
+++ cluster/cluster_memes.py
@@ -86,6 +86,7 @@ def get_jaccard_mat(memes, users):
             mat[i, j] = jaccard
 
     mat += mat.transpose() + np.eye(count)
+    logger.debug('matrix is symmetric? %s', mat == mat.transpose())
     return mat
 
 
@@ -163,6 +164,7 @@ def main():
     mat = normalize(mat, norm='max')
     mat = np.reshape(mat, (len(memes), len(memes)))
     mat += np.eye(count)
+    logger.debug('matrix is symmetric? %s', mat == mat.transpose())
 
     # Cluster the cascades.
     logger.info('clustering the cascades ...')
@@ -179,6 +181,8 @@ def main():
         indexes = indexes.astype(np.int8)
         clusters.append((val, memes_arr[indexes]))
         ordered_ind = np.concatenate((ordered_ind, indexes))
+    logger.debug('ordered_ind = ')
+    logger.debug(str(ordered_ind))
 
     # Print the clusters into the file.
     with open(os.path.join(BASEPATH, 'data', f'{DB_NAME}-clust'), 'w') as f:
@@ -189,6 +193,7 @@ def main():
 
     new_mat = mat[:, ordered_ind]
     new_mat = new_mat[ordered_ind, :]
+    logger.debug('matrix is symmetric after re-ordering? %s', new_mat == new_mat.transpose())
 
     # Calculate the clustering error.
     error = calc_error(new_mat, clusters)
