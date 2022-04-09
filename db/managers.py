Multi-state methods added. Bug fixed from CTIC. MEMM feature calculation opitimzed.

db/managers.py
@@ -205,18 +205,6 @@ class EdgeEvidenceManager(EvidenceManager):
             if i % 10000 == 0:
                 logger.info('%d documents inserted', i)
 
-    def _sequences_to_str(self, sequences):
-        if self.method == Method.BIN_MEMM:
-            return str([[(obs.astype(int).tolist(), state) for obs, state in seq] for seq in sequences])
-        else:
-            return str([[(obs.tolist(), state) for obs, state in seq] for seq in sequences])
-
-    def _str_to_sequences(self, seq_str):
-        if self.method == Method.BIN_MEMM:
-            return [[(np.fromiter(obs, bool), state) for obs, state in seq] for seq in eval(seq_str)]
-        else:
-            return [[(np.fromiter(obs, np.float64), state) for obs, state in seq] for seq in eval(seq_str)]
-
     def create_index(self):
         """
         Create index on 'user_id' key of MEMM evidences collection of the given project if does not exist.
@@ -284,9 +272,9 @@ class MEMMManager:
     def _doc_to_memm(self, doc):
         data = doc.read()
         memm_data = eval(data)
-        if self.method == Method.LONG_MEMM:
+        if self.method in [Method.LONG_MEMM, Method.MULTI_STATE_LONG_MEMM]:
             memm = LongMEMM()
-        elif self.method == Method.BIN_MEMM:
+        elif self.method in [Method.BIN_MEMM, Method.MULTI_STATE_BIN_MEMM]:
             memm = BinMEMM()
         elif self.method == Method.PARENT_SENS_TD_MEMM:
             memm = ParentTDMEMM()

diffusion/aslt.py
@@ -245,7 +245,7 @@ def calc_r(sequences, graph, phi_h, psi, user_ids, cascade_map, user_map, c_set1
     try:
         r_values = []
 
-        logger.info('\tcalculating values ...')
+        logger.debug('\tcalculating values ...')
         i = 0
         t0 = time.time()
 
@@ -315,7 +315,7 @@ class AsLT(LT):
         graph, sequences = self.project.load_or_extract_graph_seq()
 
         # Create maps from users and cascades db id's to their matrix id's.
-        logger.info('creating user and cascade id maps ...')
+        logger.debug('creating user and cascade id maps ...')
         user_ids = sorted(graph.nodes())
         user_map = {str(user_ids[i]): i for i in range(len(user_ids))}
         cascade_map = {str(train_set[i]): i for i in range(len(train_set))}
@@ -351,9 +351,9 @@ class AsLT(LT):
                 del g
                 if eco:
                     phi_h = self.project.load_param('phi_h', ParamTypes.SPARSE_LIST)
-                    logger.info('phi_h loaded')
+                    logger.debug('phi_h loaded')
 
-                logger.info('estimating r ...')
+                logger.debug('estimating r ...')
                 last_r = r
                 r_multi_processed = multi_processed and len(user_ids) < 2 * 10 ** 7
                 r = self.__calc_r_mp(sequences, graph, phi_h, psi, user_ids, train_set, cascade_map, user_map,
@@ -361,9 +361,9 @@ class AsLT(LT):
 
                 if eco:
                     phi_g = self.project.load_param('phi_g', ParamTypes.SPARSE_LIST)
-                    logger.info('phi_g loaded')
+                    logger.debug('phi_g loaded')
 
-                logger.info('estimating w ...')
+                logger.debug('estimating w ...')
                 last_w = w
                 w = self.__calc_w(sequences, graph, phi_h, phi_g, psi, user_ids, user_map, train_set, cascade_map)
 
@@ -404,12 +404,12 @@ class AsLT(LT):
         if eco:
             try:
                 psi = self.project.load_param('psi', ParamTypes.SPARSE_LIST)
-                logger.info('psi loaded')
+                logger.debug('psi loaded')
                 data_loaded = True
             except FileNotFoundError:
                 pass
         if not data_loaded:
-            logger.info('calculating psi ...')
+            logger.debug('calculating psi ...')
             psi = self.__calc_psi_mp(sequences, graph, w, r, g, train_set, cascade_map, user_map, multi_processed)
             if eco:
                 self.project.save_param(psi, 'psi', ParamTypes.SPARSE_LIST)
@@ -424,7 +424,7 @@ class AsLT(LT):
             except FileNotFoundError:
                 pass
         if not data_loaded:
-            logger.info('calculating phi_g ...')
+            logger.debug('calculating phi_g ...')
             phi_g = self.__calc_phi_g_mp(sequences, graph, w, g, train_set, cascade_map, user_map, multi_processed)
             if eco:
                 self.project.save_param(phi_g, 'phi_g', ParamTypes.SPARSE_LIST)
@@ -439,7 +439,7 @@ class AsLT(LT):
             except FileNotFoundError:
                 pass
         if not data_loaded:
-            logger.info('calculating phi_h ...')
+            logger.debug('calculating phi_h ...')
             phi_h = self.__calc_phi_h_mp(sequences, graph, w, r, h, train_set, cascade_map, user_map, multi_processed)
             if eco:
                 self.project.save_param(phi_h, 'phi_h', ParamTypes.SPARSE_LIST)
@@ -450,12 +450,12 @@ class AsLT(LT):
         if eco:
             try:
                 g = self.project.load_param('g', ParamTypes.SPARSE)
-                logger.info('g loaded')
+                logger.debug('g loaded')
                 data_loaded = True
             except FileNotFoundError:
                 pass
         if not data_loaded:
-            logger.info('calculating g ...')
+            logger.debug('calculating g ...')
             g = self.__calc_g_mp(sequences, graph, w, r, train_set, cascade_map, user_map, multi_processed)
             if eco:
                 self.project.save_param(g, 'g', ParamTypes.SPARSE)
@@ -466,12 +466,12 @@ class AsLT(LT):
         if eco:
             try:
                 h = self.project.load_param('h', ParamTypes.SPARSE)
-                logger.info('h loaded')
+                logger.debug('h loaded')
                 data_loaded = True
             except FileNotFoundError:
                 pass
         if not data_loaded:
-            logger.info('calculating h ...')
+            logger.debug('calculating h ...')
             h = self.__calc_h_mp(sequences, graph, w, r, train_set, cascade_map, user_map, multi_processed)
             if eco:
                 self.project.save_param(h, 'h', ParamTypes.SPARSE)
@@ -483,12 +483,12 @@ class AsLT(LT):
             try:
                 w = self.project.load_param(self.w_param_name, ParamTypes.SPARSE)
                 r = self.project.load_param(self.r_param_name, ParamTypes.ARRAY)
-                logger.info('w and r loaded')
+                logger.debug('w and r loaded')
                 data_loaded = True
             except FileNotFoundError:
                 pass
         if not data_loaded:
-            logger.info('initializing parameters ...')
+            logger.debug('initializing parameters ...')
             w, r = self.__set_initial_values(graph, user_ids, user_map)
             if eco:
                 self.project.save_param(w, self.w_param_name, ParamTypes.SPARSE)
@@ -518,13 +518,13 @@ class AsLT(LT):
                 cols.append(v_i)
             i += 1
             if i % (u_count // 10) == 0:
-                logger.info('%d%% done' % (i * 100 // u_count))
+                logger.debug('%d%% done' % (i * 100 // u_count))
 
         w = sparse.csc_matrix((values, [rows, cols]), shape=(u_count, u_count), dtype=np.float32)
         r = np.ones(u_count, np.float32) / 30  # approximately 1 day
         return w, r
 
-    @time_measure()
+    @time_measure('debug')
     def __calc_h_mp(self, data, graph, w, r, cascade_ids, cascade_map, user_map, multi_processed):
         u_count = len(user_map)
         c_count = len(cascade_ids)
@@ -561,7 +561,7 @@ class AsLT(LT):
         logger.debug('number of zero h: %d', np.count_nonzero(h.data == 0))
         return h
 
-    @time_measure()
+    @time_measure('debug')
     def __calc_g_mp(self, data, graph, w, r, cascade_ids, cascade_map, user_map, multi_processed):
         u_count = len(user_map)
         c_count = len(cascade_ids)
@@ -596,7 +596,7 @@ class AsLT(LT):
         g = sparse.csc_matrix((values, [rows, cols]), shape=(c_count, u_count), dtype=np.float32)
         return g
 
-    @time_measure()
+    @time_measure('debug')
     def __calc_phi_h_mp(self, data, graph, w, r, h, cascade_ids, cascade_map, user_map, multi_processed):
         c_count = len(cascade_ids)
 
@@ -628,7 +628,7 @@ class AsLT(LT):
             phi_h[cindex] = mat
         return phi_h
 
-    @time_measure()
+    @time_measure('debug')
     def __calc_phi_g_mp(self, data, graph, w, g, cascade_ids, cascade_map, user_map, multi_processed):
         c_count = len(cascade_ids)
 
@@ -661,7 +661,7 @@ class AsLT(LT):
 
         return phi_g
 
-    @time_measure()
+    @time_measure('debug')
     def __calc_psi_mp(self, data, graph, w, r, g, cascade_ids, cascade_map, user_map, multi_processed):
         c_count = len(cascade_ids)
 
@@ -694,7 +694,7 @@ class AsLT(LT):
 
         return psi
 
-    @time_measure()
+    @time_measure('debug')
     def __calc_r_mp(self, data, graph, phi_h, psi, user_ids, cascade_ids, cascade_map, user_map, multi_processed):
         u_count = len(user_ids)
         c_count = len(cascade_ids)
@@ -748,7 +748,7 @@ class AsLT(LT):
 
         return r
 
-    @time_measure()
+    @time_measure('debug')
     def __calc_w(self, data, graph, phi_h, phi_g, psi, user_ids, user_map, cascade_ids, cascade_map):
         u_count = len(user_ids)
 
@@ -769,7 +769,7 @@ class AsLT(LT):
                 for u in cascade.get_active_parents(v, graph):
                     muv_set3[(u, v)].append(m)
 
-        logger.info('\tcalculating values ...')
+        logger.debug('\tcalculating values ...')
         values = []
         rows = []
         cols = []
@@ -817,7 +817,7 @@ class AsLT(LT):
 
         w = sparse.csc_matrix((values, [rows, cols]), shape=(u_count, u_count), dtype='d')
 
-        logger.info('\tnormalizing w ...')
+        logger.debug('\tnormalizing w ...')
         w = normalize(w, axis=0, copy=False)
         w = sparse.csc_matrix((w.data, w.indices, w.indptr), shape=w.shape, dtype=np.float32)
 

memm/memm.py
@@ -105,6 +105,7 @@ class MEMM(abc.ABC):
         self.orig_indexes = orig_indexes
         self.orig_indexes_map = {self.orig_indexes[i]: i for i in range(len(self.orig_indexes))}
         self.feat_dim = Lambda.size
+        logger.debug('self.feat_dim set to %d', self.feat_dim)
 
     def get_prob(self, obs: np.ndarray, state: int, all_states: list, timers: list = None) -> float:
         """
@@ -265,6 +266,8 @@ class LongMEMM(MEMM):
     def _calc_obs_features(self, obs, states):
         obs_dim = obs.shape[1]
         feat_dim = self.feat_dim if self.feat_dim else obs.shape[0] * obs_dim + 1
+        features = np.zeros((states.size, feat_dim - 1))
+
         if np.any(states):
             features = np.zeros((states.size, feat_dim - 1))
             mults = np.array([self.td_param ** i for i in range(obs.shape[0])])
@@ -272,8 +275,6 @@ class LongMEMM(MEMM):
             flat_obs = np.multiply(mults, obs).flatten()[:feat_dim - 1]
             nonzero_indexes = np.nonzero(states)[0]
             features[nonzero_indexes, :flat_obs.size] = np.tile(flat_obs, (nonzero_indexes.size, 1))
-        else:
-            features = np.zeros((states.size, feat_dim - 1))
 
         C = obs_dim + 1
         feat_sum = np.sum(features, axis=1)
@@ -308,6 +309,21 @@ class LongMEMM(MEMM):
 
 
 class BinMEMM(MEMM):
+    def _calc_obs_features(self, obs, states):
+        obs_dim = obs.shape[1]
+        features = np.zeros((states.size, obs_dim))
+
+        if np.any(states):
+            feat = np.any(obs, axis=0)
+            nonzero_indexes = np.nonzero(states)[0]
+            features[nonzero_indexes, :] = np.tile(feat, (nonzero_indexes.size, 1))
+
+        C = obs_dim + 1
+        feat_sum = np.sum(features, axis=1)
+        last_feat = np.ones((states.size, 1)) * C - np.reshape(feat_sum, (states.size, 1))
+        features = np.concatenate((features, last_feat), axis=1)
+        return features, C
+
     def _calc_multi_obs_features(self, observations, states):
         if len(observations) != states.size:
             raise ValueError('number of observations and states must be equal')
@@ -330,20 +346,37 @@ class TDMEMM(MEMM):
         super().__init__()
         self.td_param = td_param
 
+    def _calc_obs_features(self, obs, states):
+        obs_dim = obs.shape[1]
+        features = np.zeros((states.size, obs_dim))
+
+        if np.any(states):
+            mults = np.array([self.td_param ** i for i in range(obs.shape[0])])
+            mults = np.tile(mults.reshape(obs.shape[0], 1), obs_dim)
+            feat = np.sum(np.multiply(mults, obs), axis=0)
+            nonzero_indexes = np.nonzero(states)[0]
+            features[nonzero_indexes, :] = np.tile(feat, (nonzero_indexes.size, 1))
+
+        C = obs_dim + 1
+        feat_sum = np.sum(features, axis=1)
+        last_feat = np.ones((states.size, 1)) * C - np.reshape(feat_sum, (states.size, 1))
+        features = np.concatenate((features, last_feat), axis=1)
+        return features, C
+
     def _calc_multi_obs_features(self, observations, states):
         if len(observations) != states.size:
             raise ValueError('number of observations and states must be equal')
         obs_num = len(observations)
         obs_dim = observations[0].shape[1]
-        features = []
-        for obs in observations:
-            mults = np.array([self.td_param ** i for i in range(obs.shape[0])])
-            mults = np.tile(mults.reshape(obs.shape[0], 1), obs_dim)
-            features.append(np.sum(np.multiply(mults, obs), axis=0))
-        features = np.array(features)
-        zero_state_indexes = np.where(np.logical_not(states))
-        # features[zero_state_indexes, :] = 1 - features[zero_state_indexes, :]
-        features[zero_state_indexes, :] = 0
+        features = np.zeros((obs_num, obs_dim))
+
+        for ind in range(obs_num):
+            if states[ind]:
+                obs = observations[ind]
+                mults = np.array([self.td_param ** i for i in range(obs.shape[0])])
+                mults = np.tile(mults.reshape(obs.shape[0], 1), obs_dim)
+                features[ind, :] = np.sum(np.multiply(mults, obs), axis=0)
+
         C = obs_dim + 1
         feat_sum = np.sum(features, axis=1)
         last_feat = np.ones((obs_num, 1)) * C - np.reshape(feat_sum, (obs_num, 1))
@@ -361,7 +394,9 @@ class ParentTDMEMM(TDMEMM):
             raise ValueError('number of observations and states must be equal')
         obs_num = len(observations)
         obs_dim = observations[0].shape[1]
-        td_features, _ = super()._calc_features(observations, states != 0)
+        td_features, _ = super()._calc_multi_obs_features(observations, states != 0)
+        # for obs in observations:
+        #     logger.debug('obs = \n%s', obs_to_str(obs))
         # logger.debug('states = %s', states)
         features = np.zeros((obs_num, 2 * obs_dim))
 
@@ -379,7 +414,7 @@ class ParentTDMEMM(TDMEMM):
         feat_sum = np.sum(features, axis=1)
         last_feat = np.ones((obs_num, 1)) * C - np.reshape(feat_sum, (obs_num, 1))
         features = np.concatenate((features, last_feat), axis=1)
-        # logger.debug('features = \n%s', two_d_arr_to_str(features[i, :]))
+        # logger.debug('features = \n%s', two_d_arr_to_str(features))
         return features, C
 
 
@@ -393,7 +428,7 @@ class LongParentTDMEMM(TDMEMM):
             raise ValueError('number of observations and states must be equal')
         obs_num = len(observations)
         obs_dim = observations[0].shape[1]
-        td_features, _ = super()._calc_features(observations, states != 0)
+        td_features, _ = super()._calc_multi_obs_features(observations, states != 0)
         # logger.debugv('obs_mat, state_mat =\n%s',
         #               np.concatenate((obs_mat, state_mat.reshape(state_mat.shape[0], 1)), axis=1))
         # logger.debugv('states =\n%s', states)

memm/models.py
@@ -21,6 +21,13 @@ from settings import logger
 from utils.time_utils import Timer, time_measure
 
 
+class Prediction:
+    def __init__(self, obs, prob, state=True):
+        self.obs = obs
+        self.prob = prob
+        self.state = state
+
+
 class MEMMModel(abc.ABC):
     method = None  # Define in the subclasses.
     max_iterations = 500
@@ -157,7 +164,7 @@ class MEMMModel(abc.ABC):
         count = 0
         manager = cls._get_memm_manager(project) if save_in_db else None
         graph = None
-        if issubclass(cls, ParentSensTDMEMMModel):
+        if issubclass(cls, MultiStateMEMMModel):
             graph = project.load_or_extract_graph()
 
         for key, ev in evidences.items():
@@ -288,7 +295,7 @@ class MEMMModel(abc.ABC):
         logger.debug('\n'.join(logs))
 
     @time_measure(level='debug')
-    def fit(self, iterations=None, td_param=None, multi_processed=False, eco=False):
+    def fit(self, multi_processed=False, eco=False, iterations=None, td_param=None):
         """
         Load MEMM's from DB if exist, otherwise train a MEMM for each user in the training set.
         :return: self
@@ -520,15 +527,12 @@ class NodeMEMMModel(MEMMModel, abc.ABC):
                         if obs is not None:
                             observations[child_id] = obs
                             logger.debugv('obs = \n%s', obs_to_str(obs))
-                            prob = memm.get_prob(obs, self.active_state(), self.get_states())
-                            logger.debugv('prob = %f', prob)
-                            if prob >= thr:
-                                node_id = self._get_predicted_node_id(obs, memm, tree, parents)
-                                if node_id:
-                                    logger.debugv('a reshare predicted from %s with prob %f >= %f', node_id, prob, thr)
-                                    tree.add_node(child_id, parent_id=node_id)
-                                    active_ids.add(child_id)
-                                    next_step.add(child_id)
+                            node_id, _ = self._predict_by_obs(obs, thr, memm, tree, parents)
+                            if node_id:
+                                tree.add_node(child_id, parent_id=node_id)
+                                active_ids.add(child_id)
+                                next_step.add(child_id)
+
                     else:
                         logger.debugv('user %s is already activated', child_id)
 
@@ -604,8 +608,7 @@ class NodeMEMMModel(MEMMModel, abc.ABC):
                         parents = list(graph.predecessors(child_id))
                         parents_map = {parents[i]: i for i in range(len(parents))}
                     activated = False
-                    self._last_prob = None
-                    self._last_obs = None
+                    last_pred = None
 
                     for thr in thresholds:
                         thr_active_ids = active_ids[thr]
@@ -619,7 +622,8 @@ class NodeMEMMModel(MEMMModel, abc.ABC):
                                 if not np.array_equal(obs, self._last_obs):
                                     logger.debugv('obs = \n%s', obs_to_str(obs))
                                 logger.debugv('threshold %f ...', thr)
-                                node_id = self._predict_by_obs(obs, thr, memm, trees[thr], parents)
+                                node_id, pred = self._predict_by_obs(obs, thr, memm, trees[thr], parents, last_pred)
+                                last_pred = pred
                                 if node_id:
                                     trees[thr].add_node(child_id, parent_id=node_id)
                                     thr_active_ids.add(child_id)
@@ -660,7 +664,7 @@ class NodeMEMMModel(MEMMModel, abc.ABC):
             logger.debugv('the newly active nodes are not available in the training data')
         return
 
-    def _predict_by_obs(self, obs, thr, memm, tree, obs_node_ids):
+    def _predict_by_obs(self, obs, thr, memm, tree, obs_node_ids, last_pred=None):
         """
 
         :param obs: observation
@@ -670,22 +674,22 @@ class NodeMEMMModel(MEMMModel, abc.ABC):
         :param obs_node_ids: list of node ids related to observation dimensions.
         :return: The parent id is returned if the diffusion is predicted, otherwise None.
         """
-        if np.array_equal(obs, self._last_obs):
-            prob = self._last_prob
+        if last_pred is not None and np.array_equal(obs, last_pred.obs):
+            prob = last_pred.prob
         else:
             prob = memm.get_prob(obs, self.active_state(), self.get_states())
             logger.debugv('prob = %f', prob)
             if prob == np.nan:
                 logger.warning('activation prob. of obs. %s is nan', obs)
-            self._last_obs, self._last_prob = obs, prob
+        pred = Prediction(obs, prob)
 
         if prob >= thr:
             node_id = self._get_predicted_node_id(obs, memm, tree, obs_node_ids)
             if node_id:
                 logger.debugv('a reshare predicted from %s with prob %f >= %f', node_id, prob, thr)
-            return node_id
+            return node_id, pred
 
-        return None
+        return None, pred
 
     def _get_initial_observations(self, initial_tree, max_depth_nodes, graph):
         observations = {}
@@ -887,8 +891,7 @@ class EdgeMEMMModel(MEMMModel, abc.ABC):
                         logger.debugv('testing reshare from %s to %s ...', node_id, child_id)
 
                         activated = False
-                        self._last_prob = None
-                        self._last_obs = None
+                        last_prob, last_obs = None, None
 
                         for thr in thresholds:
                             thr_active_ids = active_ids[thr]
@@ -900,10 +903,12 @@ class EdgeMEMMModel(MEMMModel, abc.ABC):
 
                                 if obs is not None:
                                     observations[thr][edge] = obs
-                                    if not np.array_equal(obs, self._last_obs):
+                                    if not np.array_equal(obs, last_obs):
                                         logger.debugv('obs = %s', obs_to_str(obs))
                                     logger.debugv('threshold %f ...', thr)
-                                    res = self._predict_by_obs(obs, edge, thr, memm, trees[thr])
+                                    res, prob = self._predict_by_obs(obs, edge, thr, memm, trees[thr], last_obs,
+                                                                     last_prob)
+                                    last_obs, last_prob = obs, prob
                                     if res:
                                         trees[thr].add_node(child_id, parent_id=node_id)
                                         thr_active_ids.add(child_id)
@@ -994,24 +999,23 @@ class EdgeMEMMModel(MEMMModel, abc.ABC):
             obs = self._update_obs(obs, cur_step_ids, index_map)
             observations[edge] = obs
 
-    def _predict_by_obs(self, obs, edge, thr, memm, tree):
-        if np.array_equal(obs, self._last_obs):
-            prob = self._last_prob
+    def _predict_by_obs(self, obs, edge, thr, memm, tree, last_obs=None, last_prob=None):
+        if last_obs is not None and np.array_equal(obs, last_obs):
+            prob = last_prob
         else:
             prob = memm.get_prob(obs, True, [False, True])
             logger.debugv('prob = %f', prob)
             if prob == np.nan:
                 logger.warning('activation prob. of obs. %s is nan', obs)
-            self._last_obs, self._last_prob = obs, prob
 
         if prob >= thr:
             if tree.get_node(edge[0]):
                 logger.debugv('a reshare predicted %f >= %f', prob, thr)
-                return edge[0]
+                return edge[0], prob
             else:
                 logger.warning('parent node %s does not exist', edge[0])
 
-        return None
+        return None, prob
 
     def _get_evid_manager(self):
         return EdgeEvidenceManager(self.project)
@@ -1019,6 +1023,53 @@ class EdgeMEMMModel(MEMMModel, abc.ABC):
     @classmethod
     def _get_memm_manager(cls, project):
         return EdgeMEMMManager(project, cls.method)
+
+
+class MultiStateMEMMModel(NodeMEMMModel, abc.ABC):
+    @staticmethod
+    def inactive_state():
+        return 0
+
+    @staticmethod
+    def active_state(node=None, graph=None):
+        parents = list(graph.predecessors(node.user_id))
+        index = parents.index(node.parent_id)
+        return index + 1
+
+    @staticmethod
+    def get_states(key=None, graph=None):
+        return list(range(graph.in_degree(key) + 1))
+
+    def _get_evid_manager(self):
+        return ParentSensEvidManager(self.project)
+
+    def _predict_by_obs(self, obs, thr, memm, tree, obs_node_ids, last_pred=None):
+        if last_pred is not None and np.array_equal(obs, last_pred.obs):
+            state, prob = last_pred.state, last_pred.prob
+        else:
+            all_states = list(range(len(obs_node_ids) + 1))
+            probs = memm.get_probs(obs, all_states)
+            new_act_indexes = np.nonzero(obs[0, :])[0]
+            if new_act_indexes.any():
+                active_states = new_act_indexes + 1
+                inactive_prob = probs[0]
+                active_prob = 1 - inactive_prob
+                active_probs = [probs[1 + i] for i in new_act_indexes]
+                i = np.argmax(active_probs)
+                state, prob = active_states[i], active_prob
+            else:
+                state, prob = 0, 0
+        pred = Prediction(obs, prob, state)
+
+        if state > 0 and prob >= thr:
+            node_id = obs_node_ids[state - 1]
+            if tree.get_node(node_id):
+                logger.debugv('a reshare predicted from %s with prob %f >= %f', node_id, prob, thr)
+                return node_id, pred
+            else:
+                logger.warning('parent node %s does not exist', node_id)
+
+        return None, pred
 
 
 class LongMEMMModel(NodeMEMMModel):
@@ -1049,12 +1100,20 @@ class LongMEMMModel(NodeMEMMModel):
         return None
 
 
+class MultiStateLongMEMMModel(LongMEMMModel, MultiStateMEMMModel):
+    method = Method.MULTI_STATE_LONG_MEMM
+
+
 class BinMEMMModel(NodeMEMMModel):
     method = Method.BIN_MEMM
 
     @staticmethod
     def get_memm_instance(td_param=None):
         return BinMEMM()
+
+
+class MultiStateBinMEMMModel(BinMEMMModel, MultiStateMEMMModel):
+    method = Method.MULTI_STATE_BIN_MEMM
 
 
 class TDMEMMModel(NodeMEMMModel):
@@ -1068,72 +1127,31 @@ class TDMEMMModel(NodeMEMMModel):
         return TDMEMM(td_param) if td_param is not None else TDMEMM()
 
 
-class ParentSensTDMEMMModel(TDMEMMModel):
+class MultiStateTDMEMMModel(TDMEMMModel, MultiStateMEMMModel):
+    method = Method.MULTI_STATE_TD_MEMM
+
+    @staticmethod
+    def get_memm_instance(td_param=None):
+        return TDMEMM(td_param) if td_param is not None else TDMEMM()
+
+
+class ParentSensTDMEMMModel(MultiStateMEMMModel):
     """
     Parent-sensitive Time-Decay MEMM model
     """
     method = Method.PARENT_SENS_TD_MEMM
 
     @staticmethod
-    def inactive_state():
-        return 0
-
-    @staticmethod
-    def active_state(node=None, graph=None):
-        parents = list(graph.predecessors(node.user_id))
-        index = parents.index(node.parent_id)
-        return index + 1
-
-    @staticmethod
-    def get_states(key=None, graph=None):
-        return list(range(graph.in_degree(key) + 1))
-
-    @staticmethod
     def get_memm_instance(td_param=None):
         return ParentTDMEMM(td_param) if td_param is not None else ParentTDMEMM()
 
-    def _get_evid_manager(self):
-        return ParentSensEvidManager(self.project)
 
-    def _predict_by_obs(self, obs, thr, memm, tree, obs_node_ids):
-        if np.array_equal(obs, self._last_obs):
-            state, prob = self._last_state, self._last_prob
-        else:
-            all_states = list(range(len(obs_node_ids) + 1))
-            probs = memm.get_probs(obs, all_states)
-            new_act_indexes = np.nonzero(obs[0, :])[0]
-            if new_act_indexes.any():
-                active_states = new_act_indexes + 1
-                inactive_prob = probs[0]
-                active_prob = 1 - inactive_prob
-                active_probs = [probs[1 + i] for i in new_act_indexes]
-                i = np.argmax(active_probs)
-                state, prob = active_states[i], active_prob
-                self._last_obs, self._last_prob, self._last_state = obs, prob, state
-            else:
-                state, prob = 0, 0
-
-        if state > 0 and prob >= thr:
-            node_id = obs_node_ids[state - 1]
-            if tree.get_node(node_id):
-                logger.debugv('a reshare predicted from %s with prob %f >= %f', node_id, prob, thr)
-                return node_id
-            else:
-                logger.warning('parent node %s does not exist', node_id)
-
-        return None
-
-
-class LongParentSensTDMEMMModel(ParentSensTDMEMMModel):
+class LongParentSensTDMEMMModel(MultiStateMEMMModel):
     method = Method.LONG_PARENT_SENS_TD_MEMM
 
     @staticmethod
     def get_memm_instance(td_param=None):
         return LongParentTDMEMM(td_param) if td_param is not None else LongParentTDMEMM()
-
-
-class FullTDMEMMModel(TDMEMMModel):
-    method = Method.FULL_TD_MEMM
 
 
 class TDEdgeMEMMModel(EdgeMEMMModel):